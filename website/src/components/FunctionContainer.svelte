3bce075d6941ebc3e96d7e13f987c6d3614fefd8
frectonz <fraol0912@gmail.com>
2022-12-24 07:45:04 +0300

    dashboard tabs

---
+++ website/src/components/FunctionContainer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <h1>
+    <span class="text-red-500 font-bold">function </span>
+    <span class="font-bold">{name}()</span>
+    <span class="text-red-500 font-bold">{"{"}</span>
+  </h1>
+  <section class="ml-10">
+    <slot />
+  </section>
+  <h1 class="text-red-500 font-bold">{"}"}</h1>
+</main>
--- website/src/pages/Dashboard.svelte
+++ website/src/pages/Dashboard.svelte
@@ -4,6 +4,14 @@
   import { user } from "../lib/store";
   import { auth } from "../lib/firebase";
   import NavBar from "../components/NavBar.svelte";
+  import FunctionContainer from "../components/FunctionContainer.svelte";
+  import {
+    Tab,
+    TabGroup,
+    TabList,
+    TabPanel,
+    TabPanels,
+  } from "@rgossiaux/svelte-headlessui";
 
   const navLinks: Link[] = [
     {
@@ -14,6 +22,8 @@
       },
     },
   ];
+
+  const tabs = ["challenges", "projects", "rewards"];
 </script>
 
 <NavBar links={navLinks} />
@@ -21,3 +31,33 @@
 <h1 class="mx-2 md:mx-5 p-4 text-xl">
   <span class="text-red-500 font-bold">sayHi</span>("{$user.displayName}");
 </h1>
+
+<TabGroup class="mx-2 md:mx-5 p-4 border border-black">
+  <TabList class="py-4 grid grid-cols-3 h-20">
+    {#each tabs as tab}
+      <Tab
+        class={({ selected }) =>
+          `border border-black ${selected && "bg-black text-white"}`}
+      >
+        <span class="text-red-500 font-bold">{tab}</span>();
+      </Tab>
+    {/each}
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <FunctionContainer name="challenges">
+        <p>challenges go here</p>
+      </FunctionContainer>
+    </TabPanel>
+    <TabPanel>
+      <FunctionContainer name="projects">
+        <p>projects go here</p>
+      </FunctionContainer>
+    </TabPanel>
+    <TabPanel>
+      <FunctionContainer name="rewards">
+        <p>rewards go here</p>
+      </FunctionContainer>
+    </TabPanel>
+  </TabPanels>
+</TabGroup>
